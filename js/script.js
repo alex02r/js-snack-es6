73ab3da67a13ede3752c7cc8e51a411f4fe4c188
alex02r <alex.giochi02@gmail.com>
2023-12-06 13:51:10 +0100

    [UP]Creazione array di oggetti

---
+++ js/script.js
@@ -0,0 +1,35 @@
+//SNACK 1
+const bikes = [
+    {
+        name : "Endurace CF SLX 8",
+        peso : "7"
+    },
+    {
+        name : "Litening Air C:68X",
+        peso : "6"
+    },
+    {
+        name : "TCR Advanced",
+        peso : "5"
+    },
+    {
+        name : "KTM Revelator",
+        peso : "10"
+    },
+    {
+        name : "Raise AXS 07",
+        peso : "8"
+    },
+    {
+        name : "Aero M20ILTD",
+        peso : "5"
+    },
+    {
+        name : "Rose X-Lite 04",
+        peso : "7"
+    },
+    {
+        name : "Scott Addict SE",
+        peso : "9"
+    }
+]
